cf3fafa6710e5c8330214b01955c21a1b834a91e
Alan Mitchell <tabb99@gmail.com>
2018-03-11 09:16:34 -0800

    Added Skeleton Dash app file.

---
+++ app.py
@@ -0,0 +1,27 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import os
+
+app = dash.Dash(__name__)
+server = app.server
+
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+app.layout = html.Div([
+    html.H2('Heat Pump Calculation: <i>BETA</i>'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['Anchorage', 'Fairbanks', 'Juneau', 'Bethel']],
+        value='Anchorage'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
