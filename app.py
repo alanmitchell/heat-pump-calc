3e1db32b149cb0ed0f5e64988d474b34bcffbffd
Alan Mitchell <tabb99@gmail.com>
2018-03-11 17:22:52 +0000

    Added __init__.py file

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ server = app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.layout = html.Div([
-    html.H2('Heat Pump Calculation: <i>BETA</i>'),
+    html.H2('Heat Pump Calculator: Under Construction'),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['Anchorage', 'Fairbanks', 'Juneau', 'Bethel']],
