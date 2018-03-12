edf3d89da468bb0d881fe6bb751d0dccca4ee550
Alan Mitchell <tabb99@gmail.com>
2018-03-12 13:32:48 -0800

    Added error logging.

--- heatpump/app.py
+++ heatpump/app.py
@@ -1,13 +1,34 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import os
+from os.path import dirname, join, realpath
+import logging, logging.handlers
 
 from . import library as lib
 
-app = dash.Dash(__name__)
-server = app.server
-server.debug = True
+app = dash.Dash(__name__)       # this is the Dash app
+server = app.server             # this is the underlying Flask app
+
+# path to tis directory
+APP_PATH = dirname(realpath(__file__))
+
+# ----- Add a file logger to application
+
+# Log file for the application
+LOG_FILE = join(APP_PATH, 'logs', 'hpc.log')
+
+# create a rotating file handler
+fh = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=200000, backupCount=5)
+fh.setLevel(logging.INFO)
+
+# create formatter and add it to the handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+
+# add the handler to the logger for the Flask app
+server.logger.addHandler(fh)
+
+# -----
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
@@ -28,4 +49,4 @@ app.layout = html.Div([
               [dash.dependencies.Input('dropdown', 'value')])
 def display_value(value):
     cty = lib.city_from_id(value)
-    return html.Pre(str(cty))
+    return html.Pre('Information for selected city:\n\n{}'.format(cty))
