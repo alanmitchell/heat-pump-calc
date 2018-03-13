17c45b0a31dae9486604c43d5e4f3d37b5ff0140
Alan Mitchell <tabb99@gmail.com>
2018-03-12 21:07:10 -0800

    Started Heat Pump model.

---
+++ heatpump/hp_model.py
@@ -0,0 +1,13 @@
+"""Provides a class to model the impact of a heat pump on
+energy use and cost.
+"""
+
+import pandas as pd
+
+class HP_model:
+
+    def __init__(self):
+        pass
+    
+    def monthly_results(self):
+        pass
