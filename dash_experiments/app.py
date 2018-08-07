66e96082567ece6ae22b66fced2603b19af7cd6e
Alan Mitchell <tabb99@gmail.com>
2018-08-07 10:15:21 -0800

    Dash experiments with reusable components.

--- dash_experiments/app.py
+++ dash_experiments/app.py
@@ -4,27 +4,68 @@ import dash_html_components as html
 
 app = dash.Dash()
 
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>Experiments</title>
+        {%favicon%}
+        {%css%}
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto">
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+        </footer>
+    </body>
+</html>
+'''
+
+def labeled_input(label, id, help_text='', input_type='number'):
+    
+    label_items = [label + ' ']
+    if len(help_text.strip()):
+        label_items.append(html.I(className="fas fa-question-circle", title=help_text))
+        
+    return html.Div(className='labeled-comp', children=[
+                html.P(children=label_items),
+                dcc.Input(id=id, type=input_type),
+            ])
+
+def input_section(label, children):
+    return [
+        html.Hr(),
+        html.Div(className='row', children=[
+            html.P(label, className='three columns section-title'),
+
+            html.Div(className='nine columns', children=children)
+        ])
+    ]
+
 app.layout = html.Div(className='container', children=[
-    html.H1(children='Hello Dash'),
+    
+    html.H1(children='Heat Pump Calculator'),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    *input_section('Location Info', [
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
+        labeled_input('Number of Indoor Units:', 
+                      'indoor-units',
+                      'Enter the number of Heat Pump Indoor Units (heads).'),
+
+        labeled_input('Floor Area of Building:', 
+                      'floor-area'),
+
+        labeled_input('Number of Building Occupants:', 
+                      'occupants',
+                      'Enter the number of people living in the building.'),            
+    ])
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    # app.run_server(debug=True)   # use on Windows computer
+    app.run_server(debug=True, host='0.0.0.0')   # use on Pixelbook
     
